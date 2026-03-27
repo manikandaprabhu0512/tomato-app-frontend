107464bb77c608ad00d0e16c73bf9ee0dab400ff
mmanikandap0512 <manikandaprabhu0512@gmail.com>
2026-03-27 15:01:27 +0530

    Add Code

---
+++ src/main.tsx
@@ -0,0 +1,20 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import { AppProvider } from "./context/AppContext.tsx";
+import "leaflet/dist/leaflet.css";
+import { SocketProvider } from "./context/SocketContext.tsx";
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
+      <AppProvider>
+        <SocketProvider>
+          <App />
+        </SocketProvider>
+      </AppProvider>
+    </GoogleOAuthProvider>
+  </StrictMode>,
+);
